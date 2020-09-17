870ee7b0f214802a5df9915c137cf7f829aec382
wantenglim <70679260+wantenglim@users.noreply.github.com>
2020-09-17 13:26:18 +0800

    Add files via upload

---
+++ ChessPiece.java
@@ -0,0 +1,12 @@
+public interface ChessPiece
+{
+    public abstract boolean isRed();
+    public abstract boolean canMove(Spot start, Spot end);
+    public abstract String pieceImage();
+    public abstract String rotatedPieceImage();
+    public abstract String pieceName();
+    public abstract void rotated();
+    public abstract boolean isRotated();
+  
+}
+
---
+++ Chevron.java
@@ -0,0 +1,55 @@
+public class Chevron implements ChessPiece
+{
+	
+    private boolean red;
+    
+    public Chevron(boolean red)
+    {
+        this.red = red;
+    }
+    
+    public boolean isRed()
+    {
+        return this.red == true;
+    }
+	
+    
+    public void rotated(){}
+    public boolean isRotated(){return false;}
+    public String pieceName()
+    {
+        if(this.isRed())
+            return "Red Chevron";
+        else 
+            return "Blue Chevron";
+    }
+    public boolean canMove(Spot start, Spot end)
+    {
+        if(end.getPiece() != null)
+        {
+            if(end.getPiece().isRed() == start.getPiece().isRed()) 
+                return false;
+        }
+        int x = Math.abs(start.getX() - end.getX());
+        int y = Math.abs(start.getY() - end.getY());
+        if((x == 2 && y == 1) || (y == 2 && x == 1)) // chevron bug
+            return true;
+        return false;
+    }
+    
+    public String pieceImage()
+    {
+        if(this.isRed())
+            return "chevronred.png" ;
+        else
+            return "chevronblue.png" ;
+    }
+    public String rotatedPieceImage()
+    {
+        if(this.isRed())
+            return "rotatedchevronred.png";
+        else
+            return "rotatedchevronblue.png";
+    }
+}
+
