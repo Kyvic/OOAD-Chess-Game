f785ea337fcfa89d14a8847b222104ff3876474b
Vickey Tan <69897444+Kyvic@users.noreply.github.com>
2020-09-18 10:09:50 +0800

    Delete WebaleChess.java

--- Code by Beh/WebaleChess.java
+++
@@ -1,9 +0,0 @@
-public class WebaleChess {
-	public static void main(String [] args){
-		View theView = new View();
-		Model theModel = new Model();
-		Controller theController = new Controller(theView, theModel);
-		
-		theView.setVisible(true); 
-	}
-}	
