0137cc857764b58e8b4da6379d9f06bd747eb50d
Vickey Tan <69897444+Kyvic@users.noreply.github.com>
2020-09-18 10:20:26 +0800

    Delete ChessPiece.java

--- Code/ChessPiece.java
+++
@@ -1,9 +0,0 @@
-public interface ChessPiece{
-    public abstract boolean isRed();
-    public abstract boolean canMove(Spot start, Spot end);
-    public abstract String pieceImage();
-    public abstract String rotatedPieceImage();
-    public abstract String pieceName();
-    public abstract void rotated();
-    public abstract boolean isRotated();
-}
